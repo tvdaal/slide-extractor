c8a3259847488ef120332f852eaa5a6632efd255
Tom van Daal <tvdaal@Toms-MBP.attlocal.net>
2021-11-14 12:42:13 -0800

    First working version of slide extractor.

--- extract_slides.py
+++ extract_slides.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-"""This script is used to extract frames from a video and same them to a PDF.
+"""This script is used to extract frames from videos and save them to a PDF.
 
 It accepts various command line arguments.
 
     Example of how to run:
 
-    python extract_slides.py <path_to_input_data_dir> <path_to_slides>.pdf
+    python extract_slides.py <path_to_input_data_dir> <out_path>.pdf
+    python extract_slides.py "/Users/tvdaal/Dropbox/Tom/CS/Data science/Coursera - MLOps specialization (2021)/Course 1 - Introduction to Machine Learning in Production/Week 1 - Overview of the ML Lifecycle and Deployment" slides.pdf
 
     For more information on the various optional parameters run:
 
@@ -15,6 +16,7 @@ It accepts various command line arguments.
 
 # Standard Python libraries:
 from argparse import ArgumentParser
+import glob
 import os
 from typing import List
 
@@ -35,12 +37,18 @@ def frames_to_pdf(im_paths: List[str], out_path: str) -> bool:
         True if PDF generation successful, otherwise False.
     """
 
-    im_paths[0].save(
+    # Save images to list:
+    for im_path in im_paths:
+        im = Image.open(im_path)
+        im_list.append(im)
+
+    # Save frames to PDF:
+    im_list[0].save(
         out_path,
         "PDF",
         resolution=100.0,
         save_all=True,
-        append_images=im_paths[1:],
+        append_images=im_list[1:],
     )
     success = os.path.isfile(out_path)
 
@@ -48,7 +56,10 @@ def frames_to_pdf(im_paths: List[str], out_path: str) -> bool:
 
 
 def video_to_frames(iteration: int, path: str, sec: int):
-    """Extracts frames from single video file.
+    """Extracts frames from a single video file into "frames" directory.
+
+    The iteration variable is simply a number that is used in the naming of
+    the saved frames.
 
     Args:
         iteration: Iteration number.
@@ -61,6 +72,16 @@ def video_to_frames(iteration: int, path: str, sec: int):
     fps = int(video.get(cv2.CAP_PROP_FPS))
     frames_thr = fps * sec
 
+    # Empty directory for frames or create it if it does not exist yet:
+    frames_dir = "frames"
+    if not os.path.exists(frames_dir):
+      os.makedirs(frames_dir)
+    else:
+        pattern = frames_dir + "/*"
+        files = glob.glob(pattern)
+        for file in files:
+            os.remove(file)
+
     # Extract frames from video, one by one:
     frames_count = 0
     success = 1
@@ -68,23 +89,22 @@ def video_to_frames(iteration: int, path: str, sec: int):
     while success:
         success, frame = video.read()
         if (frames_count % frames_thr == 0) and (frame is not None):
-            im_path = "tests/frame_{}_{}.jpg".format(iteration, frames_count)
+            frame_name = "frame_{}_{}.jpg".format(iteration, frames_count)
+            im_path = os.path.join(frames_dir, frame_name)
             cv2.imwrite(im_path, frame)
-            im = Image.open(im_path)
-            im_list.append(im)
         frames_count += 1
 
 
 def get_file_paths(input_dir: str, ext: str) -> List[str]:
     """Obtain a list of all files in the given directory tree.
 
-        Args:
-            dir: The path to the input directory.
-            ext: Extension of files to save.
+    Args:
+        input_dir: The path to the input directory.
+        ext: Extension of files to save.
 
-        Returns:
-            List of full paths to all files that are contained in the input
-            directory, including subdirectories.
+    Returns:
+        List of full paths to all files that are contained in the input
+        directory, including subdirectories.
     """
 
     list_of_files = os.listdir(input_dir)
@@ -131,7 +151,7 @@ def dir_to_pdf(input_dir: str, out_path: str, ext: str, sec: int) -> bool:
     """
 
     dir_to_frames(input_dir, ext, sec)
-    temp_dir = "test"
+    temp_dir = "frames"
     im_paths = get_file_paths(temp_dir, "jpg")
     success = frames_to_pdf(im_paths, out_path)
 
@@ -176,11 +196,9 @@ if __name__ == "__main__":
     parser = construct_argparser()
     args = parser.parse_args()
 
-    # Generate the PDF:
-    # python extract_slides.py "/Users/tvdaal/Dropbox/Tom/CS/Data science/Coursera - MLOps specialization (2021)/Course 1 - Introduction to Machine Learning in Production/Week 1 - Overview of the ML Lifecycle and Deployment" slides.pdf
-
+    # Generate the PDF from the collection of video files:
     success = dir_to_pdf(args.input_dir, args.out_path, args.ext, args.sec)
     if not success:
         print("-\n> Generation failed :(")
     else:
-        print("\n-> Generation successful! :)")
+        print("\n-> Generation successful :)")
